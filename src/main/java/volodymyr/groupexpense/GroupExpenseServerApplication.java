9ecb471c93a388a6a7362b44854ce0741ce02be3
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2022-10-04 19:39:38 +0200

    init project and add README.md

---
+++ src/main/java/volodymyr/groupexpense/GroupExpenseServerApplication.java
@@ -0,0 +1,13 @@
+package volodymyr.groupexpense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupExpenseServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupExpenseServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/volodymyr/groupexpense/GroupExpenseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package volodymyr.groupexpense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupExpenseServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
